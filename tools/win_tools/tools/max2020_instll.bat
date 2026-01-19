0766ac888774e526c77af98b1ead754277696a89
linhuan <hanhong@papegames.net>
2026-01-19 16:55:17 +0800

    更新

--- tools/win_tools/tools/max2020_instll.bat
+++ tools/win_tools/tools/max2020_instll.bat
@@ -1,3 +1,11 @@
+:: 请求管理员权限
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting admin privilege...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
 @echo off
 setlocal
 
