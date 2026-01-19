ef43a04be8777b6e70552f12fe987776a7e56959
linhuan <hanhong@papegames.net>
2026-01-19 17:53:07 +0800

    更新

--- tools/win_tools/tools/max2020_instll.bat
+++ tools/win_tools/tools/max2020_instll.bat
@@ -9,52 +9,101 @@ if %errorlevel% NEQ 0 (
     exit /b
 )
 
-:: ================== 路径配置 ==================
+:== Microsoft Visual C++ 2010 SP1 Redistributable (x86)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x86\VCRedist\2010SP1\vcredist_x86.exe /q /norestart
 
-set SRC=Z:\software\dcc\3dmax2020
-set SETUP=%SRC%\Img\Setup.exe
-set INI=%SRC%\Img\3dmax2020.ini
-set LOG=%TEMP%\Max2020_install.log
+:== Microsoft Visual C++ 2010 SP1 Redistributable (x64)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x64\VCRedist\2010SP1\vcredist_x64.exe /q /norestart
+
+:== Microsoft Visual C++ 2012 Redistributable (x86)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x86\VCRedist\2012UPD4\vcredist_x86.exe /install /quiet /norestart
+
+:== Microsoft Visual C++ 2012 Redistributable (x64)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x64\VCRedist\2012UPD4\vcredist_x64.exe /install /quiet /norestart
+
+:== Microsoft Visual C++ 2013 Redistributable (x86)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x86\VCRedist\2013\vcredist_x86.exe /q /norestart /l C:\Users\linhuan\AppData\Local\Temp\vcredist_x86_2013.log
+
+:== Microsoft Visual C++ 2013 Redistributable (x64)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x64\VCRedist\2013\vcredist_x64.exe /q /norestart /l C:\Users\linhuan\AppData\Local\Temp\vcredist_x64_2013.log
+
+:== Universal C Runtime (KB3118401)
+Z:\software\dcc\3dmax2020\Img\3rdParty\ucrt\AdUcrtInstaller.exe /S
+
+:== Microsoft Visual C++ 2015 Redistributable (x86)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x86\VCRedist\2015UPD3\vcredist_x86.exe /q /norestart
+
+:== Microsoft Visual C++ 2015 Redistributable (x64)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x64\VCRedist\2015UPD3\vcredist_x64.exe /q /norestart
+
+:== Microsoft Visual C++ 2017 Redistributable (x86)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x86\VCRedist\2017\vc_redist.x86.exe /install /quiet /norestart
+
+:== Microsoft Visual C++ 2017 Redistributable (x64)
+Z:\software\dcc\3dmax2020\Img\3rdParty\x64\VCRedist\2017\vc_redist.x64.exe /install /quiet /norestart
+
+:== .NET Framework Runtime 4.7
+Z:\software\dcc\3dmax2020\Img\3rdParty\NET\47\wcu\dotNetFramework\dotnetfx47_full_x86_x64.exe /q /norestart
 
 
+:===================================
+:: Autodesk 产品
+:===================================
+
+:== Autodesk Material Library 2020
+Z:\software\dcc\3dmax2020\Img\Content\ADSKMaterials\2020\CM\MaterialLibrary2020.msi ADSK_EULA_STATUS=#1 MUILANG=zh-cn ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" REBOOT=ReallySuppress ADSK_SETUP_EXE=1 /q
+
+:== Autodesk Material Library Base Resolution Image Library 2020
+Z:\software\dcc\3dmax2020\Img\Content\ADSKMaterials\2020\ILB\BaseImageLibrary2020.msi ADSK_EULA_STATUS=#1 MUILANG=zh-cn ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" REBOOT=ReallySuppress ADSK_SETUP_EXE=1 /q
+
+:== Autodesk Material Library Medium Resolution Image Library 2020
+Z:\software\dcc\3dmax2020\Img\Content\ADSKMaterials\2020\ILM\MediumImageLibrary2020.msi ADSK_EULA_STATUS=#1 MUILANG=zh-cn ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" REBOOT=ReallySuppress ADSK_SETUP_EXE=1 /q
+
+:== Autodesk Genuine Service
+"Z:\software\dcc\3dmax2020\Img\x64\AGS\Autodesk Genuine Service.msi" ADSK_EULA_STATUS=#1 MUILANG=zh-cn ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" REBOOT=ReallySuppress ADSK_SETUP_EXE=1 /q
+
+:== Autodesk Licensing
+Z:\software\dcc\3dmax2020\Img\x86\AdskLicensing\AdskLicensing-installer.exe --mode unattended --unattendedmodeui none
+
+:== Autodesk 3ds Max 2020
+Z:\software\dcc\3dmax2020\Img\x64\max\3dsMax.msi TRANSFORMS="Z:\software\dcc\3dmax2020\Img\x64\max\3dsMax-3dmax2020.mst" ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" REBOOT=ReallySuppress ADSK_SETUP_EXE=1 /q
+
+:== NVIDIA mental ray and IRay plugins
+Z:\software\dcc\3dmax2020\Img\x64\MENTALRAYPLUG\MentalrayPlugins2020.msi ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" /q
+
+:== Autodesk Single Sign On
+Z:\software\dcc\3dmax2020\Img\x64\AdSSO\AdSSO.msi ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" /q
+
+:== MAXtoA Arnold
+Z:\software\dcc\3dmax2020\Img\x64\MAXtoA\ArnoldPlug.msi ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" /q
+
+:== Inventor Server Engine
+Z:\software\dcc\3dmax2020\Img\x64\Inventor_Server\Inventor_Engine_Max.msi ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" /q
+
+:== SQL Server LocalDB
+Z:\software\dcc\3dmax2020\Img\x64\RvtSQLDB\SqlLocalDb.msi IACCEPTSQLLOCALDBLICENSETERMS=yes ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" /q
+
+:== Revit Interoperability
+Z:\software\dcc\3dmax2020\Img\x64\REVIT\RXM.msi INSTALLDIR="D:\Autodesk\" ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" /q
+
+:== Civil View
+Z:\software\dcc\3dmax2020\Img\x86\Comp\CV\CivilView2020.msi ADSK_SOURCE_ROOT="Z:\software\dcc\3dmax2020\Img\" /q
+
+
+:: ================== 判断安装完成 ==================
+
+echo.======================================
+echo 3ds Max 2020 Install Finished
 echo ======================================
-echo Start Install 3ds Max 2020
-echo ======================================
+start "" /wait "Z:\software\dcc\3dmax2020\Autodesk全家桶授权服务器\Autodesk选择安装修复授权文件2.exe" /q
+echo Return Code = %ERRORLEVEL%
 
-timeout /t 3 >nul
-
-:: ================== 路径检查 ==================
-
-if not exist "%SETUP%" (
-    echo ERROR: Setup.exe not found
-    pause
-    exit /b 1
-)
-
-if not exist "%INI%" (
-    echo ERROR: ini file not found
-    pause
-    exit /b 1
-)
-
-:: ================== 执行安装 ==================
-cd /d "%SRC%"
-
-"%SETUP%"  /Q /I "%INI%"
-
-set RET=%ERRORLEVEL%
-
-echo.
-echo ======================================
-echo Install Finished
-echo Return Code: %RET%
-echo ======================================
-
-if "%RET%"=="0" (
-    echo Install Success!
+if %ERRORLEVEL%==0 (
+    echo 授权工具执行成功
 ) else (
-    echo Install Failed! Error Code: %RET%
+    echo 授权工具执行失败
 )
+
 
 
 pause
