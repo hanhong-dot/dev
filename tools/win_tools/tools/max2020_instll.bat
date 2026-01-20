242a1973ffd047ac425f5acb91f7adcaebb832e4
linhuan <hanhong@papegames.net>
2026-01-20 21:23:27 +0800

    更新

--- tools/win_tools/tools/max2020_instll.bat
+++ tools/win_tools/tools/max2020_instll.bat
@@ -1,44 +1,79 @@
 @echo off
-chcp 65001 >nul
+chcp 936 >nul
 setlocal EnableDelayedExpansion
-
-rem If this file is saved as UTF-8 with BOM, chcp 65001 will display Chinese correctly.
 
 :: ================== 管理员权限 ==================
 net session >nul 2>&1
 if %errorlevel% NEQ 0 (
-    echo Requesting admin privilege...
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
     exit /b
 )
-echo ======================================
-echo 3ds Max 2020 Install Start
-echo ======================================
-cd /d "Z:\software\dcc\install\3dsmax2020\Img"
-:: ================== 安装主程序 ==================
-"Z:\software\dcc\install\3dsmax2020\Img\Setup.exe" /W /q /I "Z:\software\dcc\install\3dsmax2020\Img\3dsmax2020.ini" /language zh-cn
 
-echo Return Code = %ERRORLEVEL%
-if %ERRORLEVEL% NEQ 0 (
-    echo 3ds Max 2020 安装失败，错误码 %ERRORLEVEL%
-    pause
-    exit /b %ERRORLEVEL%
-)
-:: ================== 判断安装完成 ==================
-rem 安装完成后，运行授权工具
-rem 判断安装是否成功
+set ROOT=Z:\software\dcc\install\3dsmax2020
+set IMG=%ROOT%\Img
 
+pushd "%IMG%"
 
-echo ======================================
-echo 3ds Max 2020 Install Finished
-echo ======================================
-start "" /wait "Z:\software\dcc\AutodeskLic\AutodeskLic.exe" /q
-echo Return Code = %ERRORLEVEL%
+echo ===============================
+echo Install VC Runtime
+echo ===============================
 
-if %ERRORLEVEL%==0 (
-    echo 授权工具执行成功
-) else (
-    echo 授权工具执行失败
-)
+:: VC++ 2010
+"%IMG%\3rdParty\x86\VCRedist\2010SP1\vcredist_x86.exe" /q /norestart
+"%IMG%\3rdParty\x64\VCRedist\2010SP1\vcredist_x64.exe" /q /norestart
 
+:: VC++ 2012
+"%IMG%\3rdParty\x86\VCRedist\2012UPD4\vcredist_x86.exe" /install /quiet /norestart
+"%IMG%\3rdParty\x64\VCRedist\2012UPD4\vcredist_x64.exe" /install /quiet /norestart
+
+:: VC++ 2013
+"%IMG%\3rdParty\x86\VCRedist\2013\vcredist_x86.exe" /q /norestart
+"%IMG%\3rdParty\x64\VCRedist\2013\vcredist_x64.exe" /q /norestart
+
+:: VC++ 2015-2017
+"%IMG%\3rdParty\x86\VCRedist\2017\vc_redist.x86.exe" /install /quiet /norestart
+"%IMG%\3rdParty\x64\VCRedist\2017\vc_redist.x64.exe" /install /quiet /norestart
+
+:: UCRT
+"%IMG%\3rdParty\ucrt\AdUcrtInstaller.exe" /S
+
+:: .NET 4.7
+"%IMG%\3rdParty\NET\47\wcu\dotNetFramework\dotnetfx47_full_x86_x64.exe" /q /norestart
+
+echo ===============================
+echo Install Autodesk Licensing
+echo ===============================
+
+"%IMG%\x86\AdskLicensing\AdskLicensing-installer.exe" --mode unattended --unattendedmodeui none
+
+timeout /t 5 >nul
+
+echo ===============================
+echo Install Material Library
+echo ===============================
+
+msiexec /i "%IMG%\Content\ADSKMaterials\2020\CM\MaterialLibrary2020.msi" /qn
+msiexec /i "%IMG%\Content\ADSKMaterials\2020\ILB\BaseImageLibrary2020.msi" /qn
+msiexec /i "%IMG%\Content\ADSKMaterials\2020\ILM\MediumImageLibrary2020.msi" /qn
+
+echo ===============================
+echo Install 3ds Max 2020 Core
+echo ===============================
+
+msiexec /i "%IMG%\x64\max\3dsMax.msi" TRANSFORMS="%IMG%\x64\max\3dsMax-3dsmax2020.mst" /qn
+
+echo ===============================
+echo Install Plugins
+echo ===============================
+
+msiexec /i "%IMG%\x64\MAXtoA\ArnoldPlug.msi" /qn
+msiexec /i "%IMG%\x64\MENTALRAYPLUG\MentalrayPlugins2020.msi" /qn
+msiexec /i "%IMG%\x64\Inventor_Server\Inventor_Engine_Max.msi" /qn
+msiexec /i "%IMG%\x86\Comp\CV\CivilView2020.msi" /qn
+
+echo ===============================
+echo Install Finished
+echo ===============================
+
+popd
 pause
