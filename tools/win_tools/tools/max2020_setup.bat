be496a75297c3f51d1b18bde44b046b7dea52c45
linhuan <hanhong@papegames.net>
2026-01-24 17:07:32 +0800

    更新

--- tools/win_tools/tools/max2020_setup.bat
+++ tools/win_tools/tools/max2020_setup.bat
@@ -1,16 +1,16 @@
-
 @echo off
+chcp 65001 >nul
 setlocal EnableDelayedExpansion
 
-
+:: ===== 查找 Max2020 安装路径 =====
 set MAX_PATH=
 
 for %%K in (
-"HKLM\SOFTWARE\Autodesk\3dsMax\22.0"
-"HKLM\SOFTWARE\WOW6432Node\Autodesk\3dsMax\22.0"
+    "HKLM\SOFTWARE\Autodesk\3dsMax\22.0"
+    "HKLM\SOFTWARE\WOW6432Node\Autodesk\3dsMax\22.0"
 ) do (
     for /f "tokens=2,*" %%A in ('reg query %%K /v Installdir 2^>nul') do (
-        set MAX_PATH=%%B
+        set "MAX_PATH=%%B"
     )
 )
 
@@ -19,8 +19,7 @@ if not defined MAX_PATH (
     exit /b 1
 )
 
-echo 找到 Max2020 安装路径：
-echo %MAX_PATH%
+echo 找到 Max2020 安装路径： !MAX_PATH!
 
 :: ===== INI 源文件路径 =====
 set SRC_INI=Z:\dev\dcc\3dsmax2020\plugin.ini
@@ -31,17 +30,16 @@ if not exist "%SRC_INI%" (
 )
 
 :: ===== 复制到英文目录 =====
-if exist "%MAX_PATH%\en-US" (
-    robocopy "%~dp0" "%MAX_PATH%\en-US" plugin.ini /R:0 /W:0 /NFL /NDL
-    echo 已复制到 en-US
+if exist "!MAX_PATH!\en-US" (
+    robocopy "Z:\dev\dcc\3dsmax2020" "!MAX_PATH!\en-US" "plugin.ini" /R:0 /W:0 /NFL /NDL /NJH /NJS
+    echo 已部署
 )
 
 :: ===== 复制到中文目录 =====
-if exist "%MAX_PATH%\zh-CN" (
-    robocopy "%~dp0" "%MAX_PATH%\zh-CN" plugin.ini /R:0 /W:0 /NFL /NDL
-    echo 已复制到 zh-CN
+if exist "!MAX_PATH!\zh-CN" (
+    robocopy "Z:\dev\dcc\3dsmax2020" "!MAX_PATH!\zh-CN" "plugin.ini" /R:0 /W:0 /NFL /NDL /NJH /NJS
+    echo 已部署
 )
 
-echo max2020 部署完成
-
+echo Max2020 部署完成
 pause
