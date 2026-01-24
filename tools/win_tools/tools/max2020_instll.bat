e341916bd7ce9e04c121e4ecf5f3af2c2b0aefeb
linhuan <hanhong@papegames.net>
2026-01-24 16:08:34 +0800

    更新

--- tools/win_tools/tools/max2020_instll.bat
+++ tools/win_tools/tools/max2020_instll.bat
@@ -1,39 +1,39 @@
 @echo off
-setlocal EnableDelayedExpansion
+chcp 65001 >nul
 
-:: ===== 管理员检测 =====
+:: ===== 管理员权限检测 =====
 net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Requesting admin privileges...
+if not "%errorlevel%"=="0" (
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
     exit /b
 )
-:: ===== 切换到脚本目录 =====
-chcp 65001 >nul
-setlocal EnableDelayedExpansion
 
+cd /d "%~dp0"
 
 echo ======================================
 echo 3ds Max 2020 Install Start
 echo ======================================
+
+
 cd /d Z:\dev\dcc\3dsmax2020\Img
-:: ================== 安装主程序 ==================
-Z:\dev\dcc\3dsmax2020\Img\Setup.exe  /I  Z:\dev\dcc\3dsmax2020\Img\3dsmax2020.ini   /language zh-cn
 
-
+Setup.exe /I 3dsmax2020.ini /language zh-cn
 
 echo ======================================
 echo 3ds Max 2020 Install Finished
 echo ======================================
 
 robocopy "Z:\dev\dcc\3dsmax2020" "D:\Autodesk\3ds Max 2020" 3dsmax.exe /R:0 /W:0 /NFL /NDL
-echo 3ds Max 2020 破解文件复制完成
-:: ================== 设置环境变量 ==================
+
+echo Crack file copy done
 
 robocopy "Z:\dev\dcc\3dsmax2020" "D:\Autodesk\3ds Max 2020\en-US" plugin.ini /R:0 /W:0 /NFL /NDL
 robocopy "Z:\dev\dcc\3dsmax2020" "D:\Autodesk\3ds Max 2020\zh-CN" plugin.ini /R:0 /W:0 /NFL /NDL
-echo 3ds Max 2020 插件配置文件设置完成
+
+echo Plugin config done
+
 echo ======================================
 echo 3ds Max 2020 安装及配置完成！
 echo ======================================
-pause
+
+exit /b 0
