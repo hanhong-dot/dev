5c9d4db10f1d9921c2aac9efac027d570c626429
linhuan <hanhong@papegames.net>
2026-02-05 20:10:39 +0800

    更新

--- apps/publish/process/maya/process_rig_export_fbx.py
+++ apps/publish/process/maya/process_rig_export_fbx.py
@@ -595,11 +595,11 @@ class Porcess_RigFbx_Export(Porcess_Export):
                 if __obj:
                     _exportobjs.append(__obj[0])
 
-        # if self._asset_type.lower() in ['body'] and self._entity_name.endswith('_RM') and '_RM' not in os.path.basename(_exportfile):
-        #     __dir, __base_name = os.path.split(_exportfile)
-        #     __new_base_name = __base_name.replace('.fbx', '_RM.fbx')
-        #     _exportfile = os.path.join(__dir, __new_base_name)
-        #     _exportfile=_exportfile.replace('\\', '/')
+        if self._asset_type.lower() in ['body'] and self._entity_name.endswith('_RM') and '_RM' not in os.path.basename(_exportfile):
+            __dir, __base_name = os.path.split(_exportfile)
+            __new_base_name = __base_name.replace('.fbx', '_RM.fbx')
+            _exportfile = os.path.join(__dir, __new_base_name)
+            _exportfile=_exportfile.replace('\\', '/')
 
 
         if "_GuaranteedAnim" in _exportfile:
