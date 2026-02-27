02775262bc884b39b041b86f7bba499e22071015
linhuan <hanhong@papegames.net>
2026-02-27 20:34:41 +0800

    更新

--- apps/publish/check/maya/asset/check_item_rig_shader_same_mod.py
+++ apps/publish/check/maya/asset/check_item_rig_shader_same_mod.py
@@ -172,6 +172,11 @@ class Check(object):
                 __mdl_sg = __mdl_data[0]['sg']
                 if shader[0] != __mdl_shader:
                     cmds.rename(shader[0], __mdl_shader)
+                if sg != __mdl_sg:
+                    try:
+                        cmds.rename(sg, __mdl_sg)
+                    except:
+                        pass
             else:
                 for sg in sgs:
                     __meshs = cmds.sets(sg, q=1)
@@ -191,6 +196,11 @@ class Check(object):
                             shader = cmds.listConnections('{}.surfaceShader'.format(sg), s=1, d=0)
                             if shader[0] != __mdl_shader:
                                 cmds.rename(shader[0], __mdl_shader)
+                            if sg != __mdl_sg:
+                                try:
+                                    cmds.rename(sg, __mdl_sg)
+                                except:
+                                    pass
     def _get_meshs_from_grps(self, grps):
         meshs = []
         if grps:
